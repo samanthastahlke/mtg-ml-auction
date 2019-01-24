0ec4100d90bdbc21749feebc2e64dcbe9810629e
samanthastahlke <samanthastahlke@gmail.com>
2019-01-24 18:19:17 -0500

    Initial creation of "final" dataset.

--- MTG-PyCharm/card_collector.py
+++ MTG-PyCharm/card_collector.py
@@ -3,35 +3,74 @@ import os
 import card_csv_ops as cops
 import time
 import requests
+import pickle
 
 datapath = os.path.abspath(os.path.join(os.getcwd(), os.pardir, "data\\"))
 
 #Raw data from a JSON file containing the complete list of cards.
 datapath_json = os.path.abspath(os.path.join(datapath, "cards-raw-mtgjson.json"))
+datapath_json_sets = os.path.abspath(os.path.join(datapath, "sets-raw-mtgjson.json"))
 datapath_csv = os.path.abspath(os.path.join(datapath, "cards-raw-all.csv"))
+datapath_csv_sets = os.path.abspath(os.path.join(datapath, "sets-raw-all.csv"))
 datapath_csv_clean = os.path.abspath(os.path.join(datapath, "cards-processed.csv"))
+datapath_csv_clean_sets = os.path.abspath(os.path.join(datapath, "sets-processed.csv"))
 datapath_csv_prices = os.path.abspath(os.path.join(datapath, "cards-prices.csv"))
 datapath_csv_complete = os.path.abspath(os.path.join(datapath, "cards-complete-priced.csv"))
+datapath_rarity = os.path.abspath(os.path.join(datapath, "cards-rarity.pickle"))
 
-def convert_raw_data():
-    print("Converting JSON card data to CSV...")
+def convert_raw_data(json_filepath, csv_filepath, column_drop=[]):
+    print("Converting JSON data to CSV...")
     print("Parsing JSON...")
-    with open(datapath_json, 'r', encoding='utf8') as json:
-        json_raw = pd.read_json(json)
+    with open(json_filepath, 'r', encoding='utf8') as json_file:
+        json_raw = pd.read_json(json_file)
 
     print("Constructing dataframe...")
     json_df = pd.DataFrame(json_raw)
-
-    print("Transposing dataframe...")
     json_df = json_df.T
+
+    print("Dropping unused columns...")
+    json_df = json_df.drop(column_drop, axis=1)
+
     print(json_df)
 
     print("Exporting to CSV...")
-    json_df.to_csv(datapath_csv)
+    json_df.to_csv(csv_filepath)
 
     print("Completed data conversion from JSON to CSV.")
 
-def cleanup_csv():
+def build_rarity_dict():
+    print("Fetching card rarity data...")
+    rarity_dict = {}
+
+    print("Parsing JSON...")
+    with open(datapath_json_sets, 'r', encoding='utf8') as json_file:
+        json_raw = pd.read_json(json_file)
+
+    print("Constructing dataframe...")
+    json_df = pd.DataFrame(json_raw)
+    json_df = json_df.T
+
+    set_count = 0
+    num_sets = len(json_df.index)
+
+    print("Extracting rarity data...")
+    for set_index in json_df.index:
+
+        if set_count % 100 == 0:
+            print("Processing set " + str(set_count) + " of " + str(num_sets) + "...")
+
+        rarity_dict[set_index] = {}
+        set_list = json_df.loc[set_index]['cards']
+
+        for card in set_list:
+            rarity_dict[set_index][card['uuid']] = card['rarity']
+
+        set_count += 1
+
+    with open(datapath_rarity, 'wb') as pickle_file:
+        pickle.dump(rarity_dict, pickle_file)
+
+def cleanup_cards_csv():
     print("Cleaning up card data...")
     print("Parsing CSV...")
     csv_df = pd.read_csv(datapath_csv, index_col=0)
@@ -48,20 +87,65 @@ def cleanup_csv():
     for color in colors:
         csv_df[color] = csv_df.apply(cops.card_color, args=(color,), axis=1)
 
+    typesets = ['types', 'subtypes', 'supertypes']
+
+    print("Fixing card types...")
+    for typeset in typesets:
+        combined_col_name = 'complete' + typeset[:-1]
+        csv_df[combined_col_name] = csv_df.apply(cops.card_combine_types, args=(typeset,), axis=1)
+
+    csv_df['isReserved'] = csv_df['isReserved'] == True
+    csv_df['isAlternative'] = csv_df['isAlternative'] == True
+    csv_df['starter'] = csv_df['starter'] == True
+
     print("Dropping unused attributes...")
     csv_df = csv_df.drop(['colorIdentity',
                           'colorIndicator',
                           'colors',
                           'faceConvertedManaCost',
+                          'frameEffect',
                           'foreignData',
+                          'hand',
                           'legalities',
                           'life',
                           'loyalty',
+                          'manaCost',
+                          'name',
+                          'names',
                           'rulings',
-                          'text'], axis=1)
+                          'side',
+                          'subtypes',
+                          'supertypes',
+                          'text',
+                          'tcgplayerProductId',
+                          'tcgplayerPurchaseUrl',
+                          'types'], axis=1)
 
     print("Saving CSV...")
     csv_df.to_csv(datapath_csv_clean)
+
+def cleanup_sets_csv():
+    print("Cleaning up set data...")
+    print("Parsing CSV...")
+    csv_df = pd.read_csv(datapath_csv_sets, index_col=0)
+
+    print("Fixing release year...")
+    csv_df['releaseYear'] = csv_df.apply(cops.set_release_year, axis=1)
+
+    print("Dropping unused attributes...")
+    csv_df = csv_df.drop(['block',
+                          'boosterV3',
+                          'code',
+                          'codeV3',
+                          'isFoilOnly',
+                          'isOnlineOnly',
+                          'meta',
+                          'mtgoCode',
+                          'releaseDate',
+                          'tcgplayerGroupId'], axis=1)
+
+    print("Saving CSV...")
+    csv_df.to_csv(datapath_csv_clean_sets)
 
 def fetch_prices():
     print("Fetching price data from Scryfall...")
@@ -115,4 +199,55 @@ def fetch_prices():
     print("Saving CSV...")
     prices_df.to_csv(datapath_csv_prices)
 
-fetch_prices()
+def cleanup_prices():
+    return
+
+def complete_cards_csv():
+    print("Completing card data...")
+    print("Loading data...")
+    csv_df = pd.read_csv(datapath_csv_clean, index_col=0)
+    csv_df_sets = pd.read_csv(datapath_csv_clean_sets, index_col=0)
+    csv_prices = pd.read_csv(datapath_csv_prices, index_col=0)
+
+    with open(datapath_rarity, 'rb') as pickle_file:
+        rarity_info = pickle.load(pickle_file)
+
+    print("Building price lookup...")
+    prices_dict = {}
+
+    for index in csv_prices.index:
+        prices_dict[csv_prices.loc[index]['scryfallId']] = {}
+        prices_dict[csv_prices.loc[index]['scryfallId']]['usd'] = csv_prices.loc[index]['usd']
+        prices_dict[csv_prices.loc[index]['scryfallId']]['eur'] = csv_prices.loc[index]['eur']
+
+    print("Fixing price data...")
+    csv_df = csv_df.apply(cops.card_process_price_info, args=(prices_dict,), axis=1)
+
+    print("Fixing set data...")
+    csv_df = csv_df.apply(cops.card_process_set_info, args=(csv_df_sets,rarity_info), axis=1)
+
+    print("Adding price data...")
+
+    print("Saving CSV...")
+    csv_df.to_csv(datapath_csv_complete)
+
+#To read in and reformat data for cards.
+#convert_raw_data(datapath_json, datapath_csv)
+
+#To read in and reformat data for card sets.
+#convert_raw_data(datapath_json_sets, datapath_csv_sets, ['cards', 'tokens'])
+
+#To reformat data and clean columns from the raw card data.
+#cleanup_cards_csv()
+
+#To reformat data and clean columns from the raw set data.
+#cleanup_sets_csv()
+
+#To fetch pricing data for all cards from the Scryfall API.
+#fetch_prices()
+
+#To fetch and save rarity data for cards from set data.
+#build_rarity_dict()
+
+#To fill in the card data with pricing, rarity, and set data.
+complete_cards_csv()
--- MTG-PyCharm/card_csv_ops.py
+++ MTG-PyCharm/card_csv_ops.py
@@ -1,5 +1,21 @@
 import ast
 
+rarity_to_int = {
+    'basic' : 0,
+    'common' : 1,
+    'uncommon' : 2,
+    'rare' : 3,
+    'mythic' : 4
+}
+
+int_to_rarity = {
+    0 : 'basic',
+    1 : 'common',
+    2 : 'uncommon',
+    3 : 'rare',
+    4 : 'mythic'
+}
+
 def card_legality(row, key):
 
     legality_lookup = ast.literal_eval(row['legalities'])
@@ -14,3 +30,63 @@ def card_color(row, color):
     color_list = ast.literal_eval(row['colorIdentity'])
 
     return color in color_list
+
+def card_combine_types(row, key):
+
+    type_list = ast.literal_eval(row[key])
+
+    type_combined = ""
+
+    for type_name in type_list:
+        type_combined += type_name
+
+    return type_combined
+
+def set_release_year(row):
+
+    date_str = row['releaseDate']
+
+    if len(date_str) >= 4:
+        return date_str[:4]
+
+    return ""
+
+def card_process_price_info(row, price_info):
+
+    row['usd'] = price_info[row['scryfallId']]['usd']
+    row['eur'] = price_info[row['scryfallId']]['eur']
+
+    return row
+
+def card_process_set_info(row, set_info, rarity_info):
+
+    set_list = ast.literal_eval(row['printings'])
+    year_list = []
+    rarity_list = []
+
+    set_combined = ""
+
+    for set in set_list:
+
+        if set not in set_info.index:
+            continue
+
+        year_list.append(int(set_info.loc[set]['releaseYear']))
+
+        if row['uuid'] in rarity_info[set]:
+            rarity_list.append(rarity_to_int[rarity_info[set][row['uuid']]])
+
+        set_combined += set
+
+    row['numPrintings'] = len(set_list)
+    row['setCombined'] = set_combined
+    row['firstPrinting'] = min(year_list) if len(year_list) > 0 else -1
+    row['lastPrinting'] = max(year_list) if len(year_list) > 0 else -1
+    row['rarity'] = int_to_rarity[rarity_list[0]] if len(rarity_list) > 0 else 'UNKNOWN'
+
+    '''
+    if len(rarity_list) > 0 and min(rarity_list) != max(rarity_list):
+        print("Rarity mismatch on card ", row.index)
+    '''
+
+    return row
